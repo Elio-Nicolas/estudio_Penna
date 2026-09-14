ef021dd3b7fce1991be869af5de2ec0be72913c1
Elio Nicolas <baigorriaen83@gmail.com>
2026-09-14 15:58:18 -0300

    Actualizacion Penna

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,16 @@ import Contact from "@/componentes/Contact";
 export default function Home() {
   
   // Este efecto fuerza al navegador a subir al Hero al cargar
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+ useEffect(() => {
+  // 1. Fuerza la pantalla a subir al Hero de inmediato
+  window.scrollTo(0, 0);
+  
+  // 2. Si la URL trae "#contacto", la limpia silenciosamente sin recargar la página
+  if (window.location.hash === '#contacto') {
+    window.history.replaceState(null, '', window.location.pathname);
+  }
+}, []);
+
 
   return (
     <main className="bg-[#3B2418] text-[#F3E9D5]">
